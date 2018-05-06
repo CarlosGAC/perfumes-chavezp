0e9cec808832efbac50ea3d1cf1b7e34595952e2
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2018-05-06 13:47:53 -0500

    asd

--- app/models/postal_code.rb
+++ app/models/postal_code.rb
@@ -4,7 +4,7 @@ class PostalCode < ApplicationRecord
   
   def self.importcp
     postalcodes = []
-    CSV.foreach('/Users/carlosga/Documents/GitHub/perfumes-chavez/perfumes/app/assets/Jalisco.csv', headers: true, skip_blanks: true, encoding: "ISO8859-1:utf-8") do |row|
+    CSV.foreach("#{Rails.root}/app/assets/Jalisco.csv", headers: true, skip_blanks: true, encoding: "ISO8859-1:utf-8") do |row|
       rowh = row.to_h
       postalcodes << PostalCode.new(rowh)
     end
