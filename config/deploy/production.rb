d93b8b76ad3ee536ae0e360cf67711b414fd7d49
Carlos Alberto García de Alba Chávez <carlos.alberto.gdea.ch@gmail.com>
2018-05-06 13:02:39 -0500

    asd

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '159.65.244.45', user: 'deploy', roles: %w{app db web}
+server '104.236.14.8', user: 'deploy', roles: %w{app db web}
 
 # role-based syntax
 # ==================
